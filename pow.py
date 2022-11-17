51b51d052f90c06e473d29042602cecc90f51b3e
gamagegit <cgamage92@yahoo.com>
2022-11-16 16:04:28 -0800

    completed

---
+++ pow.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        return x ** n
+    
+    
+
+x = 2.00000
+n = 10
+
+obj = Solution()
+
+print(obj.myPow(x,n))
